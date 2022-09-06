152ff0326a2f0f54420b8d5af5418ccafc38605e
Jeremy <jeremy.shiotani@gmail.com>
2022-09-06 13:01:53 -0700

    Added middleware function, example

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 
+// uses 'logger' as middleware 
+app.use(logger)
+
+
 // req (headers, parameters, body)
 app.get('/', (req, res) => {
+    console.log(req.test);
     // res (response express app sends once req is made)
     res.render('index', {firstName: 'Jeremy', lastName: 'Shiotani'});
 });
@@ -36,7 +41,7 @@ let users = [
         color: 'unknown'
     }
 
-]
+];
 
 
 app.get('/users', (req, res) => {
@@ -44,16 +49,60 @@ app.get('/users', (req, res) => {
 });
 
 
-app.get('/users/:id', (req, res) => {
+app.param('id', getUser);
 
-    const id = req.params.id
-    for (let user of users) {
-        if (user.id == id) {
-            res.render('user', { user })
+
+app.get('/users/:id', (req, res) => {
+    // const id = req.params.id
+    // for (let user of users) {
+    //     if (user.id == id) {
+    //         res.render('user', { user });
+    //         return
+    //     };
+    // };
+    // res.send({error: `User with id ${id} does not exist`});
+
+    res.render('user', { user: req.user });
+});
+
+
+app.put('/users/:id', (req, res) => {
+    res.send('Update User');
+});
+
+
+app.delete('/users/:id', (req, res) => {
+    res.send('Delete User');
+});
+
+
+app.get('/test/:testId/:testName/:abc/:xyz', (req, res) => {
+    console.log(req.params);
+    res.send('Test');
+});
+
+
+// middleware Express function
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    req.test = 123
+    next();
+};
+
+
+function getUser(req, res, next, id) {
+    let user;
+    for (let u of users) {
+        if (u.id == id) {
+            req.user = u
         }
     }
-    res.send({error: `User with id ${id} does not exist`})
-});
+    if (req.user) {
+        next()
+    } else {
+        res.send({error: `No user with an id of ${id}`})
+    }
+};
 
 
 // this 'listens' for connections
