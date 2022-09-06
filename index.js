f29936ce36c0679a42c5c96e4844650c7267e610
Jeremy <jeremy.shiotani@gmail.com>
2022-09-06 12:24:20 -0700

    Add mock user table, get user by id, multiple params get example, etc...

--- index.js
+++ index.js
@@ -5,19 +5,54 @@ const app = express();
 const port = 3000;
 
 
-// EJS allows or embedds js to be used in html
+// EJS allows or embedds js to be used in html (view engine)
 app.set('view engine', 'ejs');
 
 
 // req (headers, parameters, body)
 app.get('/', (req, res) => {
     // res (response express app sends once req is made)
-    res.render('index', {firstName: 'Jeremy'});
+    res.render('index', {firstName: 'Jeremy', lastName: 'Shiotani'});
 });
 
 
-app.get('/abc', (req, res) => {
-    res.send('ABC!');
+let users = [
+    {
+        id: 1,
+        username: 'Jeremy',
+        age: 27,
+        color: 'blue'
+    },
+    {
+        id: 2,
+        username: 'Sarah',
+        age: 28,
+        color: 'pink'
+    },
+    {
+        id: 3,
+        username: 'UhhIdk',
+        age: 29,
+        color: 'unknown'
+    }
+
+]
+
+
+app.get('/users', (req, res) => {
+    res.render('users', { users });
+});
+
+
+app.get('/users/:id', (req, res) => {
+
+    const id = req.params.id
+    for (let user of users) {
+        if (user.id == id) {
+            res.render('user', { user })
+        }
+    }
+    res.send({error: `User with id ${id} does not exist`})
 });
 
 
